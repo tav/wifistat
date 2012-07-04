ebca20fb8e903cde6c84ff98e1c82762e9687b87
tav <tav@espians.com>
2012-07-04 12:43:36 +0100

    Initialised the base app framework.

---
+++ coffee/wifistat.coffee
@@ -0,0 +1,7 @@
+# Public Domain (-) 2012 The Wifistat Authors.
+# See the Wifistat UNLICENSE file for details.
+
+main = ->
+  alert 'hello world!'
+
+main()
---
+++ wifistat.go
@@ -0,0 +1,12 @@
+// Public Domain (-) 2012 The Wifistat Authors.
+// See the Wifistat UNLICENSE file for details.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Running Wifistat")
+}
